947e87977ed9bfd0742e80e0177b08bd2d27ab5a
Proximyst <proximyst@proximyst.com>
2017-09-30 11:49:47 +0200

    Handle /img/:id

--- src/main/java/com/proximyst/birbfetcher/reddit/application/rest/GetImageById.java
+++ src/main/java/com/proximyst/birbfetcher/reddit/application/rest/GetImageById.java
@@ -1,13 +1,20 @@
 package com.proximyst.birbfetcher.reddit.application.rest;
 
+import com.proximyst.birbfetcher.reddit.Utilities;
 import com.proximyst.birbfetcher.reddit.application.Configuration;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import lombok.RequiredArgsConstructor;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 @RequiredArgsConstructor
-public class GetImageById implements Route {
+public class GetImageById
+			implements Route {
 	private final Configuration config;
 	private final String error;
 
@@ -16,7 +23,23 @@ public class GetImageById implements Route {
 		if (request.params(":id") == null) {
 			return error;
 		}
-		// TODO: Get image by supplied ID
-		return null;
+		File directory = new File(config.getBirbDirectory());
+		if (!directory.isDirectory()) {
+			return null;
+		}
+		File id = new File(directory, request.params(":id"));
+		ByteOutputStream stream = new ByteOutputStream();
+		try(FileInputStream reader = new FileInputStream(id)) {
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = reader.read(buffer)) > 0) {
+				stream.write(buffer, 0, length);
+			}
+		} catch (IOException e) {
+			Utilities.println("Couldn't read file \"" + id + "\"!");
+			e.printStackTrace();
+			return null;
+		}
+		return stream.getBytes();
 	}
 }
